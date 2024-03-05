4058ae41f5ea6dde9cdeeb8779f2fa59e968fabd
CodeAlchemyproject <codealchemyproject@gmail.com>
2024-03-05 17:05:13 +0800

    change logo

--- _site_navbar.php
+++ _site_navbar.php
@@ -1,6 +1,6 @@
 <nav>
     <ul class="nav_bar">
-        <li><a href="./index.html"><img src="./imgs/logo_test.svg" style="width: 300px;height: auto;padding:50px 0;"></a></li>
+        <li><a href="./index.html"><img src="./imgs/icon_test.svg" style="width: 8vw;height: auto;padding:50px 0;"></a></li>
         <li><a href="./problem_list.php">題目</a></li>
         <li><a href="#">排行榜</a></li>
         <li><a href="#">競賽</a></li>
