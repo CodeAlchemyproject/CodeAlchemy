72867355d0ec1eb4d6bbd77da7d0dc7749088d3b
CodeAlchemyproject <codealchemyproject@gmail.com>
2024-03-05 22:05:08 +0800

    reset_password

--- templates/reset_password.php
+++ templates/reset_password.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resect-CodeAlchemy</title>
+    <title>Reset-CodeAlchemy</title>
     <?php include("_site_header.php"); ?>
 </head>
 
