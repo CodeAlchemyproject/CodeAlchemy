b95a43fc1c60fe67f30c56b0ed2fc3397fafe76d
CodeAlchemyproject <codealchemyproject@gmail.com>
2024-03-24 05:59:05 +0800

    app

--- app.py
+++ app.py
@@ -19,14 +19,14 @@ app = Flask(__name__)
 #加密(登入/登出)
 app.config['SECRET_KEY'] = 'itismysecretkey'
 
-#取得並篩選資料
-def get_data(sql_command):
+#取得並篩選題目資料
+def get_problem_data(sql_problem_command):
     #取得資料庫連線 
     connection = db.get_connection() 
     #產生執行sql命令的物件, 再執行sql   
     cursor = connection.cursor()
     # 這裡加篩選條件
-    cursor.execute(sql_command)
+    cursor.execute(sql_problem_command)
     #取出資料
     data = cursor.fetchall()    
     #關閉資料庫連線    
@@ -46,8 +46,8 @@ def index():
     onlinejudge = request.args.get('onlinejudge','*',type=str)
     difficulty = request.args.get('difficulty','*',type=str)
     search = request.args.get('search','*',type=str)
-    sql_command='SELECT * FROM problem'
-    data=get_data(sql_command)
+    sql_problem_command='SELECT * FROM problem'
+    data=get_problem_data(sql_problem_command)
     # 預設第一頁
     page = request.args.get('page', 1, type=int)
     # 每頁顯示15列
@@ -63,15 +63,35 @@ def index():
 @app.route('/problem',methods=['GET'])
 def problem():
     problem_id = request.args.get('problem_id',type=str)
-    sql_command=f"SELECT * FROM problem where problem_id='{problem_id}'"
-    data=get_data(sql_command)
+    sql_problem_command=f"SELECT * FROM problem where problem_id='{problem_id}'"
+    data=get_problem_data(sql_problem_command)
     print(data)
     return render_template('./problem.html',data=data)
+
+#取得並篩選使用者資料
+def get_user_data(sql_user_commond):
+    #取得資料庫連線 
+    connection = db.get_connection() 
+    #產生執行sql命令的物件, 再執行sql   
+    cursor = connection.cursor()
+    # 這裡加篩選條件
+    cursor.execute(sql_user_commond)
+    #取出資料
+    data = cursor.fetchall()    
+    #關閉資料庫連線    
+    connection.close()
+    return data
 # 登入
 @app.route('/login')
 def login():
     return render_template('./login.html')
-
+# 註冊
+@app.route('/register' ,methods=['GET'])
+def register():
+    sql_user_commond="SELECT * FROM user"
+    get_user_data(sql_user_commond)
+    print(sql_user_commond)
+    return render_template('./register.html')
 #-------------------------
 # 在主程式註冊各個服務
 #-------------------------
