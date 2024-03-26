5b86753a897d897f396de9be2c57bef1901b8c13
codealchemyproject <codealchemyproject@gmail.com>
2024-03-26 21:13:16 +0800

    1

--- 爬蟲/ZJ_submit.py
+++ 爬蟲/ZJ_submit.py
@@ -1,42 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 # 设置浏览器驱动程序的路径
-driver_path = 'C:\\Users\\My_User\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe'
+driver_path = "C:\\Users\\My_User\\Documents\\msedgedriver.exe"
 
-# 创建一个 Chrome 服务对象
-service = Service(driver_path)
-
-# 创建一个 Chrome WebDriver 对象，指定服务
-driver = webdriver.Chrome(service=service)
+# 创建一个 Edge WebDriver 对象
+options = webdriver.EdgeOptions()
+options.binary_location = 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
+driver = webdriver.Edge(options=options)
 
 # 目标 URL
 url = 'https://zerojudge.tw/Login'
 driver.get(url)
 
-# 找到帐号和密码的输入框元素，并输入数据
-account = driver.find_element(By.CSS_SELECTOR, 'input.form-control#account[type=text]')
-account.send_keys('codealchemyproject@gmail.com')
+# 根据 id 定位账号输入框，并输入账号
+account_input = driver.find_element_by_id('account')
+account_input.send_keys('codealchemyproject@gmail.com')
 
-passwd = driver.find_element(By.CSS_SELECTOR, 'input.form-control#passwd[type=password]')
-passwd.send_keys('10956CodeAlchemy')
-
-# 等待 reCAPTCHA 加载完毕
-wait = WebDriverWait(driver, 20)
-submit_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
-
-# 点击提交按钮
-submit_button.click()
-
-# 等待登录结果
-try:
-    wait.until(EC.url_to_be('https://zerojudge.tw/Index'))
-    print('登录成功！')
-except:
-    print('登录失败！')
+# 根据 id 定位密码输入框，并输入密码
+password_input = driver.find_element_by_id('passwd')
+password_input.send_keys('10956CodeAlchemy')
 
 # 关闭浏览器
 driver.quit()
