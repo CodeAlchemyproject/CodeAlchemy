e7503f0c47c4cd88c7338a3c84b3a4448132ea13
codealchemyproject <codealchemyproject@gmail.com>
2024-03-27 20:42:28 +0800

    1

--- 爬蟲/ZJ_submit.py
+++ 爬蟲/ZJ_submit.py
@@ -1,24 +1,34 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# 设置浏览器驱动程序的路径
+# 設置浏覽器驅動程序的路徑
 driver_path = "C:\\Users\\My_User\\Documents\\msedgedriver.exe"
 
-# 创建一个 Edge WebDriver 对象
+# 創建一個 Edge WebDriver 對象
 options = webdriver.EdgeOptions()
 options.binary_location = 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
-driver = webdriver.Edge(options=options)
+driver = webdriver.Edge(options=options, executable_path=driver_path)
 
-# 目标 URL
-url = 'https://zerojudge.tw/Login'
-driver.get(url)
+# 設置要搜索的關鍵詞
+search_query = "ZeroJudge"
 
-# 根据 id 定位账号输入框，并输入账号
-account_input = driver.find_element_by_id('account')
-account_input.send_keys('codealchemyproject@gmail.com')
+# 打開 Google 網頁
+driver.get("https://zerojudge.tw/ShowProblem?problemid=a001")
 
-# 根据 id 定位密码输入框，并输入密码
-password_input = driver.find_element_by_id('passwd')
-password_input.send_keys('10956CodeAlchemy')
+# 使用顯性等待，等待輸入框可見並且可交互
+search_box = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.NAME, "account"))
+)
 
-# 关闭浏览器
+# 輸入搜索關鍵詞，然後按 Enter 鍵
+search_box.send_keys(search_query)
+search_box.send_keys(Keys.RETURN)
+
+# 停留一段時間，觀察搜索結果
+input("觀察搜索結果後按 Enter 鍵關閉瀏覽器...")
+
+# 關閉瀏覽器
 driver.quit()
