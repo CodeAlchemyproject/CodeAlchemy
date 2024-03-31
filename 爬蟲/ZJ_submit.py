9338da19ee8611a3f6307336eb2ce10ddcb6d3cb
codealchemyproject <codealchemyproject@gmail.com>
2024-03-31 10:27:53 +0800

    1

--- 爬蟲/ZJ_submit.py
+++ 爬蟲/ZJ_submit.py
@@ -1,34 +1,39 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+import json
 # 設置浏覽器驅動程序的路徑
 driver_path = "C:\\Users\\My_User\\Documents\\msedgedriver.exe"
 
-# 創建一個 Edge WebDriver 對象
+# 初始化 WebDriver
 options = webdriver.EdgeOptions()
 options.binary_location = 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
-driver = webdriver.Edge(options=options, executable_path=driver_path)
 
-# 設置要搜索的關鍵詞
-search_query = "ZeroJudge"
 
-# 打開 Google 網頁
+# 創建 WebDriver
+driver = webdriver.Edge(options=options)
+
+# 將 Cookies 加載到瀏覽器中
+# 從文件中讀取 Cookies
+with open("ZJ-login.txt", "r") as file:
+    cookies_data = file.read()
+# 解析 JSON 字符串
+try:
+    cookies = json.loads(cookies_data)
+except json.JSONDecodeError:
+    print("Error: Failed to decode JSON data from file.")
+    cookies = []
+for cookie in cookies:
+    # 确保 cookie 是一个字典对象
+    if isinstance(cookie, dict):
+        driver.add_cookie(cookie)
+    else:
+        print(f"Error: Invalid cookie format - {cookie}")
+
+# 打開網頁
 driver.get("https://zerojudge.tw/ShowProblem?problemid=a001")
 
-# 使用顯性等待，等待輸入框可見並且可交互
-search_box = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.NAME, "account"))
-)
-
-# 輸入搜索關鍵詞，然後按 Enter 鍵
-search_box.send_keys(search_query)
-search_box.send_keys(Keys.RETURN)
-
-# 停留一段時間，觀察搜索結果
-input("觀察搜索結果後按 Enter 鍵關閉瀏覽器...")
-
+# 在這裡添加你的網頁操作，如點擊、填寫表單等
+time.sleep(50)
 # 關閉瀏覽器
 driver.quit()
