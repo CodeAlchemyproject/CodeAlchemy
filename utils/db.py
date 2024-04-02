4f2a38696bbdfaa18935e95a97f8b8f447d9786d
codealchemyproject <codealchemyproject@gmail.com>
2024-04-02 23:16:11 +0800

    1

--- utils/db.py
+++ utils/db.py
@@ -2,6 +2,6 @@ import mysql.connector as connector
 
 # 建立資料庫連線
 connection = connector.connect(
-        user="root", password="12345", host="127.0.0.1", database="sakila"
+        user="10956CodeAlchemy", password="C0d3@lch3mY_#2024!", host="140.131.114.242", database="113-CodeAlchemy"
     )
       
