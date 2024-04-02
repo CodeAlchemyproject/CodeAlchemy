71a67342087905778f4df4aa84a13f903ebbbe2c
codealchemyproject <codealchemyproject@gmail.com>
2024-04-02 23:00:26 +0800

    1

--- ZJ_get_problem.py
+++ ZJ_get_problem.py
@@ -1,3 +1,4 @@
+import random
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -77,8 +78,8 @@ def scrape_problem_content_and_save_to_sql_server(problem_id):
 
                 # 執行 SQL 插入語句
                 sql_insert = """
-                    INSERT INTO problem (problem_id, title, content, enter_description, output_description, example_input, example_output, difficulty, tag, solved, submission, update_time)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    INSERT INTO problem (problem_id, title, content, enter_description, output_description, example_input, example_output, difficulty, tag, solved, submission, update_time,collection)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 """
 
                 # 提供預設值
@@ -87,10 +88,11 @@ def scrape_problem_content_and_save_to_sql_server(problem_id):
                 default_solved = 0
                 default_submission = 0
                 default_update_time = datetime.now()  # 如果不提供值，則預設為 NULL
+                default_collection=0
 
                 # 使用整數值插入
                 values = ('ZJ-' + problem_id, problem_title, problem_content, problem_theinput, problem_theoutput, examples_combined_input,
-                          examples_combined_output, default_difficulty, default_tag, default_solved, default_submission, default_update_time)
+                          examples_combined_output, default_difficulty, default_tag, default_solved, default_submission, default_update_time,default_collection)
 
                 # 執行 SQL 插入
                 cursor.execute(sql_insert, values)
@@ -102,7 +104,7 @@ def scrape_problem_content_and_save_to_sql_server(problem_id):
                 cursor.close()
                 conn.close()
 
-                print(f'已將題目編號 {problem_id} 的資料儲存到 SQL Server 資料庫中')
+                print(f'已將題目編號 {problem_id} 的資料儲存到 My SQL 資料庫中')
             else:
                 print('未找到指定的 div 元素')
         except Exception as e:
@@ -111,12 +113,12 @@ def scrape_problem_content_and_save_to_sql_server(problem_id):
         print(f'網頁請求失敗，狀態碼: {response.status_code}')
 
 
-# 讀取 CSV 文件中的問題編號
+# 读取 CSV 文件中的问题编号
 csv_file_path = 'ZJ_problem_list.csv'
 with open(csv_file_path, 'r', newline='', encoding='utf-8') as csvfile:
     csv_reader = csv.reader(csvfile)
     for row in csv_reader:
         problem_id = row[0]
         scrape_problem_content_and_save_to_sql_server(problem_id)
-        time.sleep(10)
+        time.sleep(random(10,20))
 print("完成")
