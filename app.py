a7bb2b67d0b39596263192afcd6141f0c6dd9c88
CodeAlchemyproject <codealchemyproject@gmail.com>
2024-04-10 03:08:33 +0800

    app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def problem_submit():
     problem_id = data.get('problem_id')
     language = data.get('language')
     code = data.get('code')
-
+    
     # 定義語言對應的文件擴展名字典
     file_extensions = {
         'python': '.py',
@@ -96,43 +96,43 @@ def problem_submit():
     # 確保目錄存在
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-    # 寫入內容到文件中
-    with open(file_path, 'w') as file:
-        encoded_code = str(code.encode('utf-8'))
-        file.write(encoded_code)
-    # 假設有三個目標文件夾
-    target_folders = ['./crawler/src/BeAPro113', './crawler/src/TestCase2024', './crawler/src/yyyiii']
+    # # 寫入內容到文件中
+    # with open(file_path, 'w') as file:
+    #     encoded_code = str(code.encode('utf-8'))
+    #     file.write(encoded_code)
+    # # 假設有三個目標文件夾
+    # target_folders = ['./crawler/src/BeAPro113', './crawler/src/TestCase2024', './crawler/src/yyyiii']
 
-    # 將文件分配到目標文件夾中
-    common.distribute_files('./source', target_folders)
+    # # 將文件分配到目標文件夾中
+    # common.distribute_files('./source', target_folders)
 
-    # 讀取帳戶資訊
-    with open('./crawler/account.json', 'r') as file: 
-        accounts = json.load(file)['account']
+    # # 讀取帳戶資訊
+    # with open('./crawler/account.json', 'r') as file: 
+    #     accounts = json.load(file)['account']
 
-    # 多執行序
-    threads = []
-    for acc in accounts:
-        username = acc[0]
-        password = acc[1]
-        # 構建該使用者資料夾的路徑
-        user_folder_path = os.path.join('./crawler/src/', username)
-        # 檢查該資料夾是否存在並且是否為空
-        if os.path.exists(user_folder_path) and os.listdir(user_folder_path):
-            # 如果該資料夾存在並且不為空，則進行處理
-            # 將該使用者資料夾下的檔案加入執行序中
-            for file_name in os.listdir(user_folder_path):
-                file_path = os.path.join(user_folder_path, file_name)
-                if os.path.isfile(file_path):
-                    thread = threading.Thread(target=process_account, args=(username, password, language))
-                    threads.append(thread)
-                    thread.start()
-        else:
-            # 如果該資料夾不存在或者為空，則顯示相應的訊息
-            print(f"資料夾 {username} 為空，未加入執行序。")
-    # 等待所有執行序完成
-    for thread in threads:
-        thread.join()
+    # # 多執行序
+    # threads = []
+    # for acc in accounts:
+    #     username = acc[0]
+    #     password = acc[1]
+    #     # 構建該使用者資料夾的路徑
+    #     user_folder_path = os.path.join('./crawler/src/', username)
+    #     # 檢查該資料夾是否存在並且是否為空
+    #     if os.path.exists(user_folder_path) and os.listdir(user_folder_path):
+    #         # 如果該資料夾存在並且不為空，則進行處理
+    #         # 將該使用者資料夾下的檔案加入執行序中
+    #         for file_name in os.listdir(user_folder_path):
+    #             file_path = os.path.join(user_folder_path, file_name)
+    #             if os.path.isfile(file_path):
+    #                 thread = threading.Thread(target=process_account, args=(username, password, language))
+    #                 threads.append(thread)
+    #                 thread.start()
+    #     else:
+    #         # 如果該資料夾不存在或者為空，則顯示相應的訊息
+    #         print(f"資料夾 {username} 為空，未加入執行序。")
+    # # 等待所有執行序完成
+    # for thread in threads:
+    #     thread.join()
     return jsonify({'success': True})
 
 # 查詢電子郵件有沒有註冊過
