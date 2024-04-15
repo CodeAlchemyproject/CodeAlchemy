b92e026561734a61777b1a3ebc35df6cbb0e3353
codealchemyproject <codealchemyproject@gmail.com>
2024-04-15 12:03:42 +0800

    1

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import time
 from werkzeug.security import generate_password_hash,check_password_hash
 import uuid
 from queue import Queue
+import pandas as pd
+from crawler.ZJ_submit import process_account
 #-----------------------
 # 匯入各個服務藍圖
 #-----------------------
@@ -19,7 +21,7 @@ from services.customer.app import customer_bp
 from services.problem.app import problem_bp
 from services.user.app import user_bp, login_manager
 from utils import db,common
-from crawler.ZJ_submit import process_account
+
 
 #-------------------------
 # 產生主程式, 加入主畫面
@@ -101,42 +103,18 @@ def problem_submit():
     # 寫入內容到文件中
     with open(file_path, 'w') as file:
         file.write(code)
+        print(f"程式碼已成功寫入至 {file_path}")
+    process_account(language)
+    if type=="test":
+        # 讀取CSV文件
+        df = pd.read_csv('result.csv')
 
-    # 假設有兩個目標文件夾
-    target_folders = ['./crawler/src/TestCase2024','./crawler/src/BeAPro113', './crawler/src/yyyiii']
+        # 獲取最後一行
+        last_row = df.tail(1)
 
-    # 將文件分配到目標文件夾中
-    common.distribute_files('./source', target_folders)
+        print(last_row)
+        return jsonify({'success': True})
 
-    # 讀取帳戶資訊
-    with open('./crawler/account.json', 'r') as file: 
-        accounts = json.load(file)['account']
-
-    # 多執行序
-    threads = []
-    for acc in accounts:
-        username = acc[0]
-        password = acc[1]
-        # 構建該使用者資料夾的路徑
-        user_folder_path = os.path.join('./crawler/src/', username)
-        # 檢查該資料夾是否存在並且是否為空
-        if os.path.exists(user_folder_path) and os.listdir(user_folder_path):
-            # 如果該資料夾存在並且不為空，則進行處理
-            # 將該使用者資料夾下的檔案加入執行序中
-            for file_name in os.listdir(user_folder_path):
-                file_path = os.path.join(user_folder_path, file_name)
-                if os.path.isfile(file_path):
-                    thread = threading.Thread(target=process_account, args=(username, password, language))
-                    threads.append(thread)
-                    thread.start()
-        else:
-            # 如果該資料夾不存在或者為空，則顯示相應的訊息
-            print(f"資料夾 {username} 為空，未加入執行序。")
-
-    # 等待所有執行序完成
-    for thread in threads:
-        thread.join()
-    return jsonify({'success': True})
 
 # 查詢電子郵件有沒有註冊過
 @app.route('/login',methods=['GET','POST'])
