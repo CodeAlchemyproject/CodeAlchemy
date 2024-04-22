6b08f3766c824d16f7670620efce32352ee9f926
CodeAlchemyproject <codealchemyproject@gmail.com>
2024-04-22 15:48:34 +0800

    app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import uuid
 from queue import Queue
 import pandas as pd
 from crawler.ZJ_submit import process_account
+# google登入
 from authlib.integrations.flask_client import OAuth
 
 #-----------------------
@@ -159,10 +160,21 @@ def login_google():
 @app.route('/google-callback')
 def authorize():
     token = google.authorize_access_token()
-    session['token'] = token
-    print(session)
+    # 使用获取的访问令牌来获取用户的信息
+    user_info = google.get('https://www.googleapis.com/oauth2/v1/userinfo').json()
+    Email = user_info['email']
+    sql_user_command=f"SELECT * FROM user where email='{Email}'"
+    user_data=db.get_data(sql_user_command)
+    # 如果有註冊過
+    if len(user_data) == 1:
+        # 將Email存入session
+        session['Email'] = Email
+        session['logged_in']=True
+        session['User_name']=user_data[0][1]
+        return redirect('/')
     # 在這裡處理使用者資訊，例如驗證、註冊等等
-    return redirect('/')
+    else:
+        return redirect('/register')
 
 # 輸入密碼
 @app.route('/login_password',methods=['GET','POST'])
