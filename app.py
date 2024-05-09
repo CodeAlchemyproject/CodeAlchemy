64fad2e979eff92f88619412eed4f120e17dabcf
CodeAlchemyproject <codealchemyproject@gmail.com>
2024-05-09 11:35:16 +0800

    app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from services.problem.app import problem_bp
 from services.contest.app import contest_bp
 from services.feedback.app import feedback_bp
 from services.user.app import user_bp
+from services.manager.app import manager_bp
 from utils import db, common
 
 # 產生主程式, 加入主畫面
@@ -281,7 +282,8 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(problem_bp, url_prefix='/problem') 
 app.register_blueprint(contest_bp, url_prefix='/contest') 
 app.register_blueprint(feedback_bp, url_prefix='/feedback') 
-app.register_blueprint(user_bp, url_prefix='/user') 
+app.register_blueprint(user_bp, url_prefix='/user')
+app.register_blueprint(manager_bp, url_prefix='/manager') 
 #-------------------------
 # 啟動主程式
 #------------------------
---
+++ services/manager/app.py
@@ -0,0 +1,27 @@
+# 匯入必要的模組
+import os
+import uuid
+from flask import request, render_template, redirect, url_for, session
+from werkzeug.utils import secure_filename
+from flask import Blueprint
+from utils import db
+
+# 產生用戶服務藍圖
+manager_bp = Blueprint('manager', __name__)
+
+#--------------------------
+# 在user服務藍圖加入路由
+#--------------------------
+@manager_bp.route('/problem',methods=['GET',"POST"])
+def problem():
+    if request.method=="POST":
+        return render_template()
+    else:
+        problem_id = request.args.get('problem_id',type=str)
+        sql_problem_command=f"SELECT * FROM problem where problem_id='{problem_id}'"
+        problem_data=db.get_data(sql_problem_command)
+        print(problem_data)
+        example_inputs = problem_data[0][5].split('|||')
+        example_outputs = problem_data[0][6].split('|||')
+        return render_template('./manager_problem.html',data=problem_data,example_inputs=example_inputs,example_outputs=example_outputs)
+    
