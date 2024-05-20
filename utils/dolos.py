acde0ad072fe0640fe965b658166bf82eede3425
John Doe <johndoe@example.com>
2024-05-20 12:12:25 +0800

    esn

--- utils/dolos.py
+++ utils/dolos.py
@@ -6,10 +6,12 @@ def submit_to_dolos(name, zipfile_path):
    and return the URL where the resulting HTML report can be found.
    """
    response = requests.post(
-      'https://dolos.ugent.be/api/reports',
+      'http://120.97.27.246:8080/api',
       files = { 'dataset[zipfile]': open(zipfile_path, 'rb') },
       data = { 'dataset[name]': name }
    )
-   json = response.json()
-   print(json)
-   return json["html_url"]
+   print(response)
+   # json = response.json()
+   # print(json)
+   # return json["html_url"]
+submit_to_dolos('student_P.zip','dolos\\student_P.zip')
