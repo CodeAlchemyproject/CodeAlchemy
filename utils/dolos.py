56f6d5cbc849615e4c4ea62a175813fa9978d050
chouenyu0808 <chouenyu940808@gmail.com>
2024-05-26 21:28:18 +0800

    dolos

--- utils/dolos.py
+++ utils/dolos.py
@@ -1,35 +1,48 @@
-import requests # pip install requests
+import requests
 import zipfile
 import os
 import uuid
 import glob
-def search_file(problem_id):
-   file_to_compress = '../source'
-   pattern = pattern = f'*_{problem_id}*'
-   files = glob.glob(os.path.join(file_to_compress, pattern))
-   return files
+
 def create_zip(problem_id):
-    # 指定要壓縮的檔案
-   file_to_compress = 'source\\a2e64f_TIOJ-1001.py'
-   # 指定輸出 Zip 檔案的路徑
-   output_zip_path = f'dolos/{uuid.uuid4()}_{problem_id}.zip'
-   # 確保輸出的目錄存在
-   os.makedirs(os.path.dirname(output_zip_path), exist_ok=True)
-   # 創建一個新的 Zip 檔案
-   with zipfile.ZipFile(output_zip_path, 'w') as zipf:
-      # 將指定檔案添加到 Zip 檔案中
-      zipf.write(file_to_compress, os.path.basename(file_to_compress))
-   return(output_zip_path.split('/')[1],output_zip_path)
+    pattern = f'*_{problem_id}*'
+    files = glob.glob(os.path.join('source', pattern))
+    
+    # 指定輸出 Zip 檔案的路徑
+    output_zip_path = f'dolos/{uuid.uuid4()}_{problem_id}.zip'
+    
+    # 確保輸出的目錄存在
+    os.makedirs(os.path.dirname(output_zip_path), exist_ok=True)
+    
+    # 創建一個新的 Zip 檔案
+    with zipfile.ZipFile(output_zip_path, 'w') as zipf:
+        for file in files:
+            zipf.write(file, os.path.basename(file))
+    
+    return (os.path.basename(output_zip_path), output_zip_path)
 
 def submit_to_dolos(name, zipfile_path):
-   """
-   Submit a ZIP-file to the Dolos API for plagiarism detection
-   and return the URL where the resulting HTML report can be found.
-   """
-   response = requests.post(
-      'https://dolos.ugent.be/api/reports',
-      files = { 'dataset[zipfile]': open(zipfile_path, 'rb') },
-      data = { 'dataset[name]': name }
-   )
-   json = response.json()
-   return json["html_url"]
+    """
+    Submit a ZIP-file to the Dolos API for plagiarism detection
+    and return the URL where the resulting HTML report can be found.
+    """
+    # 提交 ZIP 檔案到 Dolos API
+    with open(zipfile_path, 'rb') as zip_file:
+        response = requests.post(
+            'https://dolos.ugent.be/api/reports',
+            files={'dataset[zipfile]': zip_file},
+            data={'dataset[name]': name}
+        )
+    
+    # 解析 JSON 響應
+    json_response = response.json()
+    
+    # 刪除已提交的 ZIP 檔案
+    try:
+        os.remove(zipfile_path)
+        print(f'已刪除檔案: {zipfile_path}')
+    except Exception as e:
+        print(f'刪除檔案 {zipfile_path} 時發生錯誤: {e}')
+    
+    # 返回 HTML 報告的 URL
+    return json_response["html_url"]
